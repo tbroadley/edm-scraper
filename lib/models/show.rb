9c0c3cea24b3caf1fb9f2ce7aa9980187cb0af6e
Thomas Broadley <buriedunderbooks@hotmail.com>
2017-02-25 11:25:15 -0500

    more validations on Show

--- lib/models/show.rb
+++ lib/models/show.rb
@@ -1,5 +1,6 @@
 require_relative '../../environment'
 
 class Show < ActiveRecord::Base
+  validates :name, :venue, :start_date, :end_date, :url, :new, presence: true
   validates :name, uniqueness: { scope: [:venue, :start_date, :end_date] }
 end
