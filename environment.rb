0739d538ae8aa0ddf0e7ee5fa7b1b0d365b62d2f
Thomas Broadley <buriedunderbooks@hotmail.com>
2017-02-25 14:00:58 -0500

    use environment variables more effectively

--- environment.rb
+++ environment.rb
@@ -11,7 +11,7 @@ Dir.glob('./lib/*').each do |folder|
 end
 
 # tells AR what db file to use
-if (ENV['DATABASE_ENV'] || 'development') == 'development'
+if ENV['DATABASE_ENV'] == 'development'
   ActiveRecord::Base.establish_connection(
     adapter: 'postgresql',
     host: 'localhost',
@@ -19,7 +19,5 @@ if (ENV['DATABASE_ENV'] || 'development') == 'development'
     database: 'edm-scraper',
   )
 else
-  ActiveRecord::Base.establish_connection(
-    'postgres://hjfohnpcmkyinw:3fa8c5ca1d5d9640b840644ea05841f9088551bdf8a43a8e0f811cf09e68a3f0@ec2-23-21-96-70.compute-1.amazonaws.com:5432/dfak5idb6r86qh'
-  )
+  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
 end
