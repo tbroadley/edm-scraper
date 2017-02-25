5b343a62e8a70a403106d06a683e810628f8b0bc
Thomas Broadley <buriedunderbooks@hotmail.com>
2017-02-25 14:34:30 -0500

    send email!

---
+++ lib/email_builders.rb
@@ -0,0 +1,35 @@
+require 'erb'
+
+require_relative '../environment'
+require_relative 'models/show'
+
+class String
+  def truncate
+    length > 40 ? "#{self[0..39]}..." : self
+  end
+end
+
+def email_subject
+  first, second, third = Show.unseen.map(&:name).map(&:truncate)
+
+  case Show.unseen.count
+  when 1
+    "#{first} is"
+  when 2
+    "#{first} and #{second} are"
+  when 3
+    "#{first}, #{second}, and #{third} are"
+  else
+    "#{first}, #{second}, #{third}, and #{Show.unseen.count - 3} others are"
+  end + " coming to Toronto!"
+end
+
+def email_body
+  b = binding
+  b.local_variable_set(:shows, Show.unseen)
+  ERB.new(File.read('lib/mailers/show_mailer.html.erb')).result(b)
+end
+
+def format_date(date)
+  date.strftime("%A, %B %-d")
+end
--- lib/models/show.rb
+++ lib/models/show.rb
@@ -3,4 +3,6 @@ require_relative '../../environment'
 class Show < ActiveRecord::Base
   validates :name, :venue, :start_date, :end_date, :url, :new, presence: true
   validates :name, uniqueness: { scope: [:venue, :start_date, :end_date] }
+
+  scope :unseen, -> { where(new: true) }
 end
