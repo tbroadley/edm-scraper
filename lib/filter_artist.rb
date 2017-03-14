f898a1a144992f2f4d052acf9401877c594227e9
Thomas Broadley <buriedunderbooks@hotmail.com>
2017-03-13 22:06:30 -0400

    fixing things

--- lib/filter_artist.rb
+++ lib/filter_artist.rb
@@ -2,6 +2,6 @@ require_relative '../config/filter_artists_list'
 
 def filter_artist?(show_name)
   FILTER_ARTISTS_LIST.map(&:downcase).any? do |artist|
-    show_name.downcase.split(' ').include?(artist)
+    (artist.downcase.split(' ') - show_name.downcase.split(' ')).empty?
   end
 end
--- lib/models/show.rb
+++ lib/models/show.rb
@@ -1,10 +1,13 @@
 require_relative '../../environment'
 
 class Show < ActiveRecord::Base
-  validates :name, :venue, :start_date, :end_date, :new, :filter, presence: true
+  validates :name, :venue, :start_date, :end_date, presence: true
+  validates :new, :filter, inclusion: [true, false]
   validates :name, uniqueness: { scope: [:venue, :start_date, :end_date] }
 
   scope :unseen, -> {
-    where(new: true).select { |show| !show.filter || filter_artist?(show.name) }
+    where(new: true)
+      .order(start_date: :asc, end_date: :asc, name: :asc)
+      .select { |show| !show.filter || filter_artist?(show.name) }
   }
 end
