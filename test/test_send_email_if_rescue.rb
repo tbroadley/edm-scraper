35eb26b5c3421d404fe35861c516ef19a0a0bcea
Thomas Broadley <buriedunderbooks@hotmail.com>
2017-11-26 10:18:39 -0500

    Add tests for send_email_if_rescue

---
+++ test/test_send_email_if_rescue.rb
@@ -0,0 +1,31 @@
+require 'minitest/autorun'
+require 'mocha/mini_test'
+
+require_relative '../lib/send_email_if_rescue'
+
+class TestParseDateString < Minitest::Test
+  def test_send_email_if_rescue_sends_email_when_exception_is_raised
+    script_name = 'test'
+
+    request_mock = mock()
+    request_mock.expects(:send).once
+
+    PostageApp::Request.expects(:new).once.with do |method, options|
+      method == :send_message && options[:headers][:subject] == "EDM Scraper: error while running script '#{script_name}'"
+    end.returns(request_mock)
+
+    send_email_if_rescue(script_name) do
+      raise 'Test exception'
+    end
+  end
+
+  def test_send_email_if_rescue_does_not_send_email_if_no_exception_raised
+    request_mock = mock()
+    request_mock.expects(:send).never
+
+    PostageApp::Request.expects(:new).never.returns(request_mock)
+
+    send_email_if_rescue('test') do
+    end
+  end
+end
