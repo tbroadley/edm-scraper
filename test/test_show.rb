25ca4e5da12f45fd655ba4c75bf04e6026975f34
Thomas Broadley <buriedunderbooks@hotmail.com>
2017-11-27 06:57:38 -0500

    Add tests for Show#to_s

---
+++ test/test_show.rb
@@ -0,0 +1,26 @@
+require 'date'
+require 'minitest/autorun'
+require 'mocha/mini_test'
+
+require_relative '../lib/models/show'
+
+class TestShow < Minitest::Test
+  def test_show_to_s_if_start_date_and_end_date_are_same
+    name = 'Test show'
+    venue = 'Club 1'
+    date = DateTime.new(2005, 1, 1)
+    show = Show.new(name: name, venue: venue, start_date: date, end_date: date)
+
+    assert_equal "#{name} at #{venue} on #{date}", show.to_s
+  end
+
+  def test_show_to_s_if_start_date_and_end_date_are_different
+    name = 'Test show'
+    venue = 'Club 1'
+    start_date = DateTime.new(2005, 1, 1)
+    end_date = DateTime.new(2005, 1, 5)
+    show = Show.new(name: name, venue: venue, start_date: start_date, end_date: end_date)
+
+    assert_equal "#{name} at #{venue} from #{start_date} to #{end_date}", show.to_s
+  end
+end
