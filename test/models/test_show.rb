d8eed3b1d926c5bbc53deb797617dc3016915c52
Thomas Broadley <buriedunderbooks@hotmail.com>
2017-11-27 18:43:19 -0500

    Add more Show validation tests

--- test/models/test_show.rb
+++ test/models/test_show.rb
@@ -5,33 +5,17 @@ require 'mocha/mini_test'
 require_relative '../../lib/models/show'
 
 class TestShow < Minitest::Test
-  def test_show_is_valid
+  def test_show_without_new_or_url_is_valid
     date = DateTime.new(2005, 1, 1)
-    show = Show.new(name: 'Test', venue: 'Test', start_date: date, end_date: date, new: true, filter: false)
+    show = Show.new(name: 'Test', venue: 'Test', start_date: date, end_date: date, filter: false)
     assert show.valid?
   end
 
-  def test_show_without_name_is_invalid
+  def test_show_with_non_boolean_filter_is_valid
     date = DateTime.new(2005, 1, 1)
-    show = Show.new(venue: 'Test', start_date: date, end_date: date)
-    refute show.valid?
-  end
-
-  def test_show_without_venue_is_invalid
-    date = DateTime.new(2005, 1, 1)
-    show = Show.new(name: 'Test', start_date: date, end_date: date)
-    refute show.valid?
-  end
-
-  def test_show_without_name_is_invalid
-    show = Show.new(name: 'Test', venue: 'Test', start_date: DateTime.new(2005, 1, 1))
-    refute show.valid?
-  end
-
-  def test_show_with_non_boolean_new_is_invalid
-    date = DateTime.new(2005, 1, 1)
-    show = Show.new(name: 'Test', venue: 'Test', start_date: date, end_date: date, new: 'not a boolean')
-    refute show.valid?
+    show = Show.new(name: 'Test', venue: 'Test', start_date: date, end_date: date, filter: 'not a boolean')
+    assert show.valid?
+    assert_equal true, show.filter
   end
 
   def test_show_to_s_if_start_date_and_end_date_are_same
@@ -51,5 +35,21 @@ class TestShow < Minitest::Test
     show = Show.new(name: name, venue: venue, start_date: start_date, end_date: end_date)
 
     assert_equal "#{name} at #{venue} from #{start_date} to #{end_date}", show.to_s
+  end
+end
+
+show_properties = {
+  name: 'Test',
+  venue: 'Test',
+  start_date: DateTime.new(2005, 1, 1),
+  end_date: DateTime.new(2005, 1, 1),
+  filter: true,
+}
+
+show_properties.each_key do |key|
+  TestShow.send(:define_method, "test_show_without_#{key}_is_invalid") do
+    modified_show_properties = show_properties.clone
+    modified_show_properties.delete(key)
+    refute Show.new(modified_show_properties).valid?
   end
 end
